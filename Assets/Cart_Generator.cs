813e72f8f5ea61248dd7b6262d5b1f4969579fbe
Gracjan <gacqq@wp.pl>
2018-04-28 17:08:29 +0200

    początek algorytm 12

--- Assets/Cart_Generator.cs
+++ Assets/Cart_Generator.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 public class Card_Generator : MonoBehaviour {
 
-    private Shape card_pattern;
+    public enum Shape_Sizes { size1x1, size2x1 };
+
+    private Shape[,] card_pattern;
     private Difficulty_Modifiers difficulty_modifires;
+
+    private Shape_Sizes[] shape_sizes_array = new Shape_Sizes[2] { Shape_Sizes.size1x1, Shape_Sizes.size2x1 };
+
+    private Shape_Sizes current_shape_size;
 
     public void Generate_Card(Difficulty_Modifiers incoming_difficulty_modifires) {
 
@@ -32,9 +38,43 @@ public class Card_Generator : MonoBehaviour {
 
     private void Generate_Card12() {
 
+        card_pattern = new Shape[3, 4];
+
+        for (int i = 0; i < difficulty_modifires.Number_of_figures; i++) {
+
+            current_shape_size = Roll_Shape_Sizes();
+
+            switch (current_shape_size) {
+
+                case Shape_Sizes.size1x1:
+
+                    break;
+
+                case Shape_Sizes.size2x1:
+
+                    break;
+
+                default:
+
+                    Debug.Log("Error missing rolled shape size");
+                    Debug.Log(current_shape_size);
+                    break;
+            }
+        }
     }
 
     private void Generate_Card70() {
+
+        card_pattern = new Shape[7, 10];
+
+    }
+
+    private Shape_Sizes Roll_Shape_Sizes() {
+
+        return shape_sizes_array[Random.Range(0, 1)];
+    }
+
+    private void Find_Empty_Space_12_1x1() {
 
     }
 }
--- Assets/Shape.cs
+++ Assets/Shape.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Shape : MonoBehaviour {
 
+    private bool is_occupied = false;
+    protected bool is_rotative;
 }
 
 public class Shape_1x1 : Shape {
@@ -15,18 +17,34 @@ public class Shape_2x1 : Shape {
 
 public class Square : Shape_1x1 {
 
+    public Square() {
+
+        this.is_rotative = false;
+    }
 }
 
 public class Triangle : Shape_1x1 {
 
+    public Triangle() {
+
+        this.is_rotative = true;
+    }
 }
 
 public class Circle : Shape_1x1 {
 
+    public Circle() {
+
+        this.is_rotative = false;
+    }
 }
 
 public class Rectangle : Shape_2x1 {
 
+    public Rectangle() {
+
+        this.is_rotative = false;
+    }
 }
 
 
