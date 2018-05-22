3f85c84a37732e551e5be5d2f72c26c8006da729
Dannylach <dannylach@wp.pl>
2018-05-22 21:45:53 +0200

    Improved Custom game mode options

--- Assets/Custom_Mode_Script.cs
+++ Assets/Custom_Mode_Script.cs
@@ -23,6 +23,35 @@ public class Custom_Mode_Script : MonoBehaviour
         returnButton.onClick.AddListener(ReturnOnClick);
 	}
 
+    
+
+    void PlayOnClick()
+    {
+        var difficultyModifiers = SettingDificulty();
+        var cardGenerator = gameObject.AddComponent<Card_Generator>() as Card_Generator;
+
+        var col = new List<Shape.Figures_Colours>
+        {
+            Shape.Figures_Colours.Dark_Blue,
+            Shape.Figures_Colours.Red,
+            Shape.Figures_Colours.Orange
+        };
+
+        difficultyModifiers.Set_Figures_Colours(col);
+
+        cardGenerator.Generate_Card(difficultyModifiers);
+    }
+
+    void ReturnOnClick()
+    {
+
+    }
+
+    void AllowedMistakesSliderOnValueChange(float arg)
+    {
+        AllowedMistakesText.text = AllowedMistakesSlider.value.ToString();
+    }
+
     void SetInitialValue()
     {
         NumberOfFiguresInputField.text = PlayerPrefs.GetInt("NumberOfFigures").ToString();
@@ -33,10 +62,9 @@ public class Custom_Mode_Script : MonoBehaviour
         AllowedMistakesText.text = AllowedMistakesSlider.value.ToString();
     }
 
-    void PlayOnClick()
+    Difficulty_Modifiers SettingDificulty()
     {
         var difficultyModifiers = gameObject.AddComponent<Difficulty_Modifiers>() as Difficulty_Modifiers;
-        var cardGenerator = gameObject.AddComponent<Card_Generator>() as Card_Generator;
 
         difficultyModifiers.Cart_type = CardSizeDropdown.value == 12
             ? Difficulty_Modifiers.Cart_Type.Cart_Type12
@@ -64,32 +92,12 @@ public class Custom_Mode_Script : MonoBehaviour
         PlayerPrefs.SetString("CardType", difficultyModifiers.Cart_type.ToString());
         PlayerPrefs.SetInt("NumberOfFigures", difficultyModifiers.Number_of_figures);
         PlayerPrefs.SetInt("NumberOfMistakes", difficultyModifiers.Number_of_mistakes);
-        PlayerPrefs.SetInt("ColoursOnlyMechanic", int.Parse(difficultyModifiers.Colours_only_mechanic.ToString()));
-
-        var col = new List<Shape.Figures_Colours>
-        {
-            Shape.Figures_Colours.Dark_Blue,
-            Shape.Figures_Colours.Red,
-            Shape.Figures_Colours.Orange
-        };
-
-        difficultyModifiers.Set_Figures_Colours(col);
-
-        cardGenerator.Generate_Card(difficultyModifiers);
+        PlayerPrefs.SetInt("ColoursOnlyMechanic", difficultyModifiers.Colours_only_mechanic.ToString() == "TRUE" ? 1 : 0);
+        return difficultyModifiers;
     }
 
-    void ReturnOnClick()
-    {
-
-    }
-
-    void AllowedMistakesSliderOnValueChange(float arg)
-    {
-        AllowedMistakesText.text = AllowedMistakesSlider.value.ToString();
-    }
-
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
