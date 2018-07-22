f2f23b735ed4b049d92e645be9e1ae86a952bfd9
Norbert Wołowiec <nokem61@gmail.com>
2018-07-22 16:15:18 +0200

    GameChoose

---
+++ Assets/Cube.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Cube : MonoBehaviour, IPointerClickHandler
+{
+    private int _randomNumber;
+    private int _randomSide;
+    private Image _image;
+    public Sprite ArrowSprite;
+    public Sprite[] Sprites;
+    private Animator _animator;
+    private bool toRandom = true;
+    public GameChooseSceneControllerScript GameChooseSceneControllerScript;
+    public Vector2 ArrowLeftPosition;
+    public Vector2 ArrowRightPosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _randomNumber = GameChooseSceneControllerScript.RandomNumber;
+	    _randomSide = GameChooseSceneControllerScript.RandomSide;
+	    _image = GetComponent<Image>();
+	    _animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+    void SetSpriteByRandom()
+    {
+        Debug.Log("Setting sprite y random number: " + _randomNumber);
+        Destroy(_animator);
+        _image.sprite = Sprites[_randomNumber];
+        CreateArrow(_randomSide);
+    }
+
+    void CreateArrow(int side)
+    {
+        GameObject arrowGameObject = new GameObject("Arrow");
+        arrowGameObject.transform.SetParent(transform.parent);
+        Image arrowImage = arrowGameObject.AddComponent<Image>();
+        arrowImage.sprite = ArrowSprite;
+        arrowImage.rectTransform.sizeDelta = new Vector2(240, 180);
+        if (side == 0)
+        {
+            arrowImage.transform.localPosition = ArrowLeftPosition;
+
+        }
+        else
+        {
+            arrowImage.transform.localPosition = ArrowRightPosition;
+            arrowImage.transform.Rotate(0, 0, 180);
+        }
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!toRandom) return;
+        _animator.SetTrigger("drawNumber");
+        toRandom = false;
+    }
+}
---
+++ Assets/GameChooseSceneControllerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameChooseSceneControllerScript : MonoBehaviour
+{
+    public int RandomNumber { get; private set; }
+    public int RandomSide { get; private set; }
+    public CardToChooseScript[] CardToChooseScripts;
+
+    // Use this for initialization
+	void Start ()
+	{
+	    RandomNumber = Random.Range(0, 6);
+	    RandomSide = Random.Range(0, 2);
+	    Debug.Log("Random number: " + RandomNumber + " Random side: " + RandomSide);
+	    int correctCardindex;
+	    if (RandomSide == 0)
+	    {
+	        correctCardindex = RandomNumber;
+	    }
+	    else
+	    {
+	        correctCardindex = CardToChooseScripts.Length - RandomNumber - 1;
+	    }
+	    CardToChooseScripts[correctCardindex].CorrectCard = true;
+	    Debug.Log("Correct card: " + correctCardindex);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
